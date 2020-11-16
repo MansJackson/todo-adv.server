f7def12b325bca92c99ec0452a2c565e1c2cc9ae
Måns Jackson <mans.jackson@gmail.com>
2020-11-16 15:34:19 +0100

    trying to fix samesite error

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -61,6 +61,7 @@ router.post('/login', (req: Request, res: Response) => {
         jwt.sign({ id: user.id, name: user.name, email: user.email }, process.env.JWT_SECRET),
         { maxAge: 360000 * 24 * 7, httpOnly: false },
       );
+      res.setHeader('SameSite', 'none');
       res.status(200).json({ message: 'User signed in succesfully' });
     }
   }
